aa2ffe422d82d99724c948868e50592cfacd4b41
CodeLuminary <victorijoni@yahoo.com>
2021-09-29 23:26:23 +0100

    Front end login setup 2

--- InventoryMangement/Controllers/HomeController.cs
+++ InventoryMangement/Controllers/HomeController.cs
@@ -7,6 +7,9 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace InventoryMangement.Controllers
@@ -30,9 +33,14 @@ namespace InventoryMangement.Controllers
         public IActionResult login(IFormCollection collection)
         {
             string email = HttpContext.Request.Form["email"];
-            return Content(email);
-            //var data = new {userName = collection.Get("email")};
-            //return View();
+            string password = HttpContext.Request.Form["password"];
+            //return Content(email);
+            var data = new {userName = email,password=password};
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var responseMessage = client.PostAsJsonAsync("http://localhost:46229/api/user/authenticate", data).Result; //please make sure to change this to production url when you go live
+            var responseStr = responseMessage.Content.ReadAsStringAsync().Result;
+            return Content(responseStr);
         }
 
         public IActionResult Privacy()
