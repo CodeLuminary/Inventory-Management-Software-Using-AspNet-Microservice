2cab0d3b33205ca5ea43c883d59a3944c785e872
CodeLuminary <victorijoni@yahoo.com>
2021-10-10 20:31:16 +0100

    Javascript added for userpanel

---
+++ InventoryMangement/wwwroot/js/router.js
@@ -0,0 +1,9 @@
+﻿const routes = [
+    /* Put your routes here */
+    /*{ path: "/", view: Dashboard },
+    { path: "/posts", view: Posts },
+    { path: "/posts/:id", view: PostView },
+    { path: "/settings", view: Settings }*/
+]
+
+export default routes;
